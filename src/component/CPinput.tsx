15fd552b622d05e5dcb4cd7cb2dd90ee73514322
darkkshah <128061778+darkkshah@users.noreply.github.com>
2023-10-25 06:19:34 +0500

    Add files via upload

---
+++ src/component/CPinput.tsx
@@ -0,0 +1,11 @@
+import TextField from '@mui/material/TextField';
+export default function CPInput(props:any) {
+    let {id,label,variant,onChange,type, value ,style } = props;
+  return (
+    <div>
+      <TextField style={style} id={id} value={value} label={label} variant={variant} onChange={onChange} type={type} />
+    </div>
+  )
+}
+// Id="standard-basic" //filled-basic // outlined-basic
+// variant="standard"  // filled // outlined
